da43cd7b73aad84bc5b54ec051d849a660bc7ae8
lcy0x1 <arthurlcy0x1@gmail.com>
2021-11-03 15:13:51 -0700

    complete training code

--- gym_vehicle/gym_vehicle/envs/vehicle_env.py
+++ gym_vehicle/gym_vehicle/envs/vehicle_env.py
@@ -1,5 +1,6 @@
 import gym
 import math
+import pkg_resources
 from gym import error, spaces, utils
 from gym.utils import seeding
 import json
@@ -7,43 +8,70 @@ import json
 
 class VehicleAction:
 
-    def __init__(self, env):
+    def __init__(self, env, arr):
         self.motion = [[0 for _ in range(env.node)] for _ in range(env.node)]
         self.price = [[0 for _ in range(env.node)] for _ in range(env.node)]
-        pass
+        ind = 0
+        for i in range(env.node):
+            for j in range(env.node):
+                if i == j:
+                    continue
+                self.motion[i][j] = arr[ind]
+                ind = ind + 1
+        for i in range(env.node):
+            for j in range(env.node):
+                if i == j:
+                    continue
+                self.price[i][j] = arr[ind]
+                ind = ind + 1
 
 
 class VehicleEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, seed):
-        self.random = seeding.np_random(seed)
-        self.config = json.load(open("./setup.py"))
+    def __init__(self):
+        self.config = json.load(open(pkg_resources.resource_filename(__name__, "./config.json")))
         self.node = self.config["node"]
         self.vehicle = self.config["vehicle"]
         self.poisson_param = self.config["poisson_param"]
         self.operating_cost = self.config["operating_cost"]
         self.waiting_penalty = self.config["waiting_penalty"]
+        self.price_discretization = self.config["price_discretization"]
+        self.queue_size = self.config["queue_size"]
+        self.overflow = self.config["overflow"]
         self.vehicles = [0 for _ in range(self.node)]
         self.queue = [[0 for _ in range(self.node)] for _ in range(self.node)]
-        self.reset()
+        self.random = None
+        self.observation_space = spaces.MultiDiscrete(
+            [self.vehicle + 1 for _ in range(self.node)] +
+            [self.queue_size + 1 for _ in range(self.node * (self.node - 1))])
+        self.action_space = spaces.MultiDiscrete(
+            [self.vehicle + 1 for _ in range(self.node * (self.node - 1))] +
+            [self.price_discretization + 1 for _ in range(self.node * (self.node - 1))])
 
-    def step(self, action: VehicleAction):
+    def seed(self, seed=None):
+        self.random, _ = seeding.np_random(seed)
+
+    def step(self, act):
+        action = VehicleAction(self, act)
         utility = 0
         for i in range(self.node):
             for j in range(self.node):
                 if i == j:
                     continue
-                veh_motion = action.motion[i][j]
+                veh_motion = min(action.motion[i][j], self.vehicles[i])  # TODO truncate invalid action
                 self.vehicles[i] = self.vehicles[i] - veh_motion
                 self.vehicles[j] = self.vehicles[j] + veh_motion
                 self.queue[i][j] = max(0, self.queue[i][j] - veh_motion)
                 utility = utility - veh_motion * self.operating_cost
                 utility = utility - self.queue[i][j] * self.waiting_penalty
-                request = self.random.poisson(self.poisson_param * (1 - action.price[i][j]))
-                self.queue[i][j] = self.queue[i][j] + request
-                utility = utility + request * action.price[i][j]
-        return utility
+                price = action.price[i][j] / self.price_discretization
+                request = self.random.poisson(self.poisson_param * (1 - price))
+                act_req = min(request, self.queue_size - self.queue[i][j])
+                utility = utility - (request - act_req) * self.overflow
+                self.queue[i][j] = self.queue[i][j] + act_req
+                utility = utility + act_req * action.price[i][j]
+        return self.to_observation(), utility, False, {}
 
     def reset(self):
         for i in range(self.node):
@@ -53,9 +81,23 @@ class VehicleEnv(gym.Env):
         for i in range(self.vehicle):
             pos = self.random.randint(0, self.node)
             self.vehicles[pos] = self.vehicles[pos] + 1
+        return self.to_observation()
 
     def render(self, mode='human'):
         pass
 
     def close(self):
         pass
+
+    def to_observation(self):
+        arr = [0 for _ in range(self.node * self.node)]
+        for i in range(self.node):
+            arr[i] = self.vehicles[i]
+        ind = self.node
+        for i in range(self.node):
+            for j in range(self.node):
+                if i == j:
+                    continue
+                arr[ind] = self.queue[i][j]
+                ind = ind + 1
+        return arr
---
+++ training/main.py
@@ -0,0 +1,41 @@
+import gym
+import numpy as np
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.utils import set_random_seed
+import gym_vehicle
+
+
+def make_env(env_id, rank, seed=0):
+    def _init():
+        env = gym.make(env_id)
+        env.seed(seed + rank)
+        print("initial state: ",env.reset())
+        print("Action Space: ",env.observation_space)
+        print("Observation Space: ", env.action_space)
+        return env
+
+    set_random_seed(seed)
+    return _init
+
+
+if __name__ == "__main__":
+    env_id = "vehicle-v0"
+    num_cpu = 1  # Number of processes to use
+    # Create the vectorized environment
+    env = DummyVecEnv([make_env(env_id, i) for i in range(num_cpu)])
+
+    # Stable Baselines provides you with make_vec_env() helper
+    # which does exactly the previous steps for you.
+    # You can choose between `DummyVecEnv` (usually faster) and `SubprocVecEnv`
+    # env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
+
+    model = PPO('MlpPolicy', env, verbose=1)
+    model.learn(total_timesteps=25_000)
+
+    obs = env.reset()
+    for _ in range(1000):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
