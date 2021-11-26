d22844f6436e359f2063fb2aea2071235c67aed9
lcy0x1 <arthurlcy0x1@gmail.com>
2021-11-26 00:24:31 -0800

    finish

--- render/demo.py
+++ render/demo.py
@@ -62,6 +62,35 @@ class Station(RenderObject):
         self.sprite = Circle(Point(self.x, self.y), 10)
         self.sprite.draw(win)
 
+        self.queue_sprite = [[Image(Point((self.x + 20 * (j + 1)), (self.y - 20)),
+                                    f"./gostation{i + 1}.png") for j in range(4)] for i in range(3)]
+        self.Queue = []
+        self.newQueue = []
+        self.nowindex = index
+
+        Text(Point(self.x, self.y), f"{index + 1}").draw(win)
+
+    def drawQueue(self, start):
+
+        if start == 1:
+            old_sprite_ind = 0
+            for dst in range(3):
+                if self.nowindex == dst:
+                    continue
+                for q in range(self.Queue[dst]):
+                    self.queue_sprite[dst][old_sprite_ind].undraw()
+                    old_sprite_ind = old_sprite_ind + 1
+
+        sprite_ind = 0
+        for dst in range(3):
+            if self.nowindex == dst:
+                continue
+            for q in range(self.newQueue[dst]):
+                self.queue_sprite[dst][sprite_ind].draw(win)
+                sprite_ind = sprite_ind + 1
+
+        self.Queue = self.newQueue
+
     def move_vehicle(self, target, n):
         for _ in range(n):
             self.vehicles[0].set_index(target)
@@ -83,7 +112,7 @@ class Cars(RenderObject):
     def init_pos(self, i):
         self.index = i
         self.x = stations[self.index].x
-        self.y = stations[self.index].y
+        self.y = stations[self.index].y + 30 + len(stations[self.index].vehicles) * 20
         self.target_x = self.x
         self.target_y = self.y
         stations[self.index].vehicles.append(self)
@@ -92,7 +121,7 @@ class Cars(RenderObject):
         old_station = self.index
         self.index = i
         self.target_x = stations[self.index].x
-        self.target_y = stations[self.index].y
+        self.target_y = stations[self.index].y + 30 + len(stations[self.index].vehicles) * 20  # TODO time order problem
         stations[old_station].vehicles.remove(self)
         stations[self.index].vehicles.append(self)
 
@@ -108,6 +137,43 @@ class Cars(RenderObject):
         self.y = self.target_y
 
 
+class Price():
+    def __init__(self):
+        self.price: List[List] = [[None for _ in range(3)] for _ in range(3)]
+        self.lines: List[List] = [[None for _ in range(3)] for _ in range(3)]
+
+        for src in range(3):
+            for dst in range(3):
+                if src == dst:
+                    continue
+                ssrc = stations[src]
+                sdst = stations[dst]
+                psrc = Point(ssrc.x * 0.9 + sdst.x * 0.1, ssrc.y * 0.9 + sdst.y * 0.1)
+                pdst = Point(sdst.x * 0.9 + ssrc.x * 0.1, sdst.y * 0.9 + ssrc.y * 0.1)
+                dy = sdst.y - ssrc.y
+                dx = sdst.x - ssrc.x
+                line = Line(psrc, pdst)
+                line.move(-dy * 0.1, dx * 0.1)
+                line.setArrow("last")
+                line.draw(win)
+                self.lines[src][dst] = line
+
+    def drawprice(self, price, start):
+        if start == 1:
+            for src in range(3):
+                for dst in range(3):
+                    if src == dst:
+                        continue
+                    self.price[src][dst].undraw()
+        for src in range(3):
+            for dst in range(3):
+                if src == dst:
+                    continue
+                lc = self.lines[src][dst].getCenter()
+                self.price[src][dst] = Text(Point(lc.x, lc.y + 20), "$" + str(round(price[src][dst], 2)))
+                self.price[src][dst].draw(win)
+
+
 def moving():
     _action, _state = container.model.predict(container.state)
     action = VehicleAction(container.env, _action)
@@ -117,15 +183,18 @@ def moving():
             if i == j or action.motion[i][j] == 0:
                 continue
             stations[i].move_vehicle(j, action.motion[i][j])
+    return action.price, action.motion
 
 
 if __name__ == "__main__":
     container = Container()
+    start = 0
 
     win = GraphWin("My Circle", 800, 800)
 
     stations = [Station(i, 3) for i in range(3)]
     cars = [Cars() for i in range(3)]
+    prices = Price()
 
     ind = 0
     for i in range(3):
