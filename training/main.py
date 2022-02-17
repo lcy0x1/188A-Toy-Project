1e4204cfb6426d80e388cf23eec3e42eee4e7420
vfinley <59670689+vfinley@users.noreply.github.com>
2022-02-16 17:49:58 -0800

    data_n3_v4_edge2 conditions
    2/16/2022

--- training/main.py
+++ training/main.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
     for i in range(20):
         model.learn(total_timesteps=1_000_000)
-        model.save(f"./data_n3_v4_edge1/{i + 1}mil")
+        model.save(f"./data_n3_v4_edge2/{i + 1}mil")
         sums = 0
         obs = env.reset()
         for _ in range(1000):
